2a3062c478d4dc9b760abe74bf7eab303243ea10
Nathaniel Crivelli <nathaniel.crivelli@mrjaffesclass.com>
2023-12-06 14:22:57 -0800

    hello

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { byName, byYear } = require('us-baby-names-2');
 const app = express();
 const port = 3000;
 
@@ -7,6 +8,40 @@ app.use(function (req, res, next) {
     req.timestamp = new Date().toString();
     next();
 })
+  
+// Transform the data object elements into an
+// HTML table
+const formatToHTML = function(dataArr) {
+    // If dataArr is undefined or null, make an empty array
+    if (!dataArr) {
+      dataArr = [];
+    }
+    // Use the Array.map function to convert each record 
+    // into an HTML table element.
+    dataArr = dataArr.map(item => {
+      // Create the HTML here
+      let html = '<tr>'
+      html += (item.year) ? '<td>'+item.year+'</td>' : '';
+      html += (item.name) ? '<td>'+item.name+'</td>' : '';
+      html += (item.sex) ? '<td>'+item.sex+'</td>' : '';
+      html += (item.count) ? '<td>'+item.count+'</td>' : '';
+      html += '</tr>';
+      return html
+    })
+    // Now join all the elements together inside the 
+    // <table><tbody> elements.
+    return '<table><tbody>'+
+      dataArr.join('')+'</tbody></table>';
+  }
+  
+  // Transform name with first character capitalized and the 
+  // rest lower case
+  const fixName = function(name) {
+    let newName = name.toLowerCase();
+    newName = newName.charAt(0).toUpperCase() +
+      newName.substr(1)
+    return newName
+  }
   
 
 app.get('/', (req, res) => {
